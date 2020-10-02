ca49072faa386cdf297c3a4cd31dc97b27016dd2
k.yuka <61102173+baron2020@users.noreply.github.com>
2020-10-03 01:19:37 +0900

    Add files via upload

--- 電卓/Calculator.java
+++ 電卓/Calculator.java
@@ -27,19 +27,32 @@ public class Calculator {
 		JPanel p = new JPanel();
 		frame.setContentPane(p);
 		p.setLayout(null);
-		//計算表示ラベル
-		JLabel label1 = new JLabel();//"表示画面"
-		label1.setFont(new Font("MSゴシック", Font.PLAIN, 24));
-		//配置x,配置y,サイズwidth,サイズheight
-		int displayWidth = 480;//表示画面の横幅
-		int displayHeight = 80;//表示画面の高さ
+
+		//ラベル関連
+		JLabel[] JLabelArray = new JLabel[4];
+		//ラベルのテキスト
+		String[] jbValueArray = { "入力：", "", "結果：", "" };
+		//ラベルのインスタンス生成
+		for (int i = 0; i < JLabelArray.length; i++) {
+			JLabelArray[i] = new JLabel(jbValueArray[i]);
+			JLabelArray[i].setFont(new Font("MSゴシック", Font.PLAIN, 24));
+		}
+		JLabel inputDisplay = JLabelArray[1];//"入力式表示ラベル"
+		JLabel resultDisplay = JLabelArray[3];//"計算結果表示ラベル"
+		int displayW = 480;//表示画面の横幅
+		int displayH = 80;//表示画面の高さ
 		int displayMarginX = 110;//余白X
-		int displayMarginY = 70;//余白Y
-		label1.setBounds(displayMarginX, displayMarginY, displayWidth, displayHeight);
-		//ボタンのテキスト
+		int displayMarginY = 55;//余白Y
+		//配置x,配置y,サイズwidth,サイズheight
+		JLabelArray[0].setBounds(displayMarginX, displayMarginY, 80, displayH);
+		JLabelArray[2].setBounds(displayMarginX, displayMarginY * 2, 80, displayH);
+		inputDisplay.setBounds(displayMarginX + 75, displayMarginY, displayW, displayH);
+		resultDisplay.setBounds(displayMarginX + 75, displayMarginY * 2, displayW, displayH);
+
+		//ボタン関連
+		JButton[] JButtonArray = new JButton[20];
 		String[] bValueArray = { "Ｃ", "√", "％", "÷", "７", "８", "９", "×", "４", "５", "６", "－",
 				"１", "２", "３", "＋", "０", "００", "．", "＝" };
-		JButton[] JButtonArray = new JButton[20];
 		//ボタン20種類のインスタンス生成とイベントの設定
 		for (int i = 0; i < JButtonArray.length; i++) {
 			JButtonArray[i] = new JButton(bValueArray[i]);
@@ -49,25 +62,26 @@ public class Calculator {
 			JButtonArray[i].addActionListener(e -> {
 				//System.out.println(bValue + "ボタンを押しました。");
 				if (bValue.equals("Ｃ")) {
-					label1.setText("");
+					inputDisplay.setText("");
+					resultDisplay.setText("");
 					return;
 				}
 				if (bValue.equals("＝")) {
-					equalResult(label1);
+					equalResult(inputDisplay);
 					return;
 				}
-				String disp = label1.getText() + bValue;
-				label1.setText(disp);
+				String display = inputDisplay.getText() + bValue;
+				inputDisplay.setText(display);
 			});
 		}
 		//ボタン配置関連(X4行＊Y5段)
 		int bWidth = 120;//ボタンの横幅
 		int bHeight = 80;//ボタンの高さ
 		int bMarginX = 110;//余白X
-		int bMarginY = 150;//余白Y
+		int bMarginY = 200;//余白Y
 		//ボタン20個の配置(X4行＊Y5段)
 		int[] setX = { bMarginX, 230, 350, 470 };
-		int[] setY = { bMarginY, 230, 310, 390, 470 };
+		int[] setY = { bMarginY, 280, 360, 440, 520 };
 		int bIndex = 0;//0～19個のボタンの添え字
 		for (int y = 0; y < setY.length; y++) {
 			for (int x = 0; x < setX.length; x++) {
@@ -76,46 +90,49 @@ public class Calculator {
 				bIndex++;
 			}
 		}
-		p.add(label1);//ラベルの配置
+		p.add(JLabelArray[0]);
+		p.add(JLabelArray[2]);
+		p.add(inputDisplay);//入力式ラベルの配置
+		p.add(resultDisplay);//計算結果ラベルの配置
 		for (int i = 0; i < JButtonArray.length; i++) {
 			p.add(JButtonArray[i]);//ボタンの配置
 		}
 		frame.setVisible(true);//画面に見えるようにする
 	}
-	//=が押された時の処理
+
+	//＝が押された時の処理
 	public void equalResult(JLabel target) {
-		String tempUse = target.getText();//表示されている計算式
+		String tempSiki = target.getText();//表示されている計算式
 		List<Integer> iSiki = new ArrayList<Integer>();//数字
-		List<String> sSiki = new ArrayList<String>();//記号
-		List<Integer> kigouIti = new ArrayList<Integer>();//記号の位置
-		//System.out.println("計算式" + tempUse);
-		//System.out.println("計算式の長さ" + tempUse.length());
-		if (tempUse.length() == 0) {
+		List<String> enzansiArray = new ArrayList<String>();//記号
+		List<Integer> enzansiPositionArray = new ArrayList<Integer>();//記号の位置
+		//System.out.println("計算式" + tempSiki);
+		//System.out.println("計算式の長さ" + tempSiki.length());
+		if (tempSiki.length() == 0) {
 			//計算式が空の時
 			return;
 		}
 		//正規表現
 		String regex1 = "([０-９]+)";//全角数字
-		//String regex2 = "([0-9]+)";//半角数字
 		Pattern p1 = Pattern.compile(regex1);
-		Matcher m1 = p1.matcher(tempUse);
+		Matcher m1 = p1.matcher(tempSiki);
 		//1個以上の数字のパターンを抽出
 		while (m1.find()) {
 			iSiki.add(Integer.parseInt(m1.group()));
 			//System.out.println("抽出数字:" + m1.group());
 		}
-		String regex3 = "([^０-９])";//半角数字以外
+		String regex3 = "([^０-９])";//全角数字以外
 		Pattern p2 = Pattern.compile(regex3);
-		Matcher m2 = p2.matcher(tempUse);
+		Matcher m2 = p2.matcher(tempSiki);
 		//数字以外のパターンを抽出＆一致した位置を取得
 		while (m2.find()) {
-			kigouIti.add(m2.start());
-			sSiki.add(m2.group());
+			enzansiArray.add(m2.group());
+			enzansiPositionArray.add(m2.start());
 			//System.out.println("一致した位置:" + m2.start() + ",抽出記号:" + m2.group());
 		}
-		target.setText(tempUse);
+		target.setText(tempSiki);
 		System.out.println("数字配列:" + iSiki);
-		System.out.println("記号配列:" + sSiki);
-		System.out.println("記号位置配列:" + kigouIti);
+		System.out.println("演算子配列:" + enzansiArray);
+		System.out.println("演算子位置配列:" + enzansiPositionArray);
 	}
 }
